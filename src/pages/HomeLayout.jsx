0278f591b26c19070b979bf8316289ae090fec4f
Megagig <megagigsoftwaresolutions.com>
2024-09-08 09:58:41 +0100

    Nested Routes

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const HomeLayout = () => {
   return (
     <div>
       li<h2>Home layout</h2>
       <Link to="/about">About page</Link>
+      <Outlet />
     </div>
   );
 };
